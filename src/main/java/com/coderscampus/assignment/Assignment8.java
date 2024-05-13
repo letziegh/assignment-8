090c3a34de4c3aec339580444d7f38f5db2497d9
letziegh <148257470+letziegh@users.noreply.github.com>
2024-05-13 16:57:44 -0400

    Commit 2 for Assignment 8

--- src/main/java/com/coderscampus/assignment/Assignment8.java
+++ src/main/java/com/coderscampus/assignment/Assignment8.java
@@ -5,11 +5,19 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Assignment8 {
+    public static void main(String[] args) {
+        Assignment8 assignment8 = new Assignment8();
+        assignment8.processNumbers();
+    }
+
     private List<Integer> numbers = null;
     private AtomicInteger i = new AtomicInteger(0);
 
@@ -31,7 +39,7 @@ public class Assignment8 {
      * of Integers. However, it can only return 1000 records at a time. You will
      * need to call this method 1,000 times in order to retrieve all 1,000,000
      * numbers from the list
-     * 
+     *
      * @return Integers from the parsed txt file, 1,000 numbers at a time
      */
     public List<Integer> getNumbers() {
@@ -57,5 +65,32 @@ public class Assignment8 {
         System.out.println("Done Fetching records " + start + " to " + (end));
         return newList;
     }
+    public void processNumbers(){
+        int numOfCalls = numbers.size()/1000;
+        List<CompletableFuture<List<Integer>>> futures = new ArrayList<>();
 
+        for(int j=0;j<numOfCalls;j++){
+            futures.add(CompletableFuture.supplyAsync(this::getNumbers));
+        }
+
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+
+        CompletableFuture<List<List<Integer>>> allPageContentsFuture = allFutures.thenApply(v ->
+                futures.stream().map(CompletableFuture::join).collect(Collectors.toList())
+        );
+
+        CompletableFuture<Map<Integer,Long>> frequencyMapFuture = allPageContentsFuture.thenApply(lists -> {
+            List<Integer> allNumbers = lists.stream().flatMap(List::stream).collect(Collectors.toList());
+            return allNumbers.stream().collect(Collectors.groupingBy(num -> num, Collectors.counting()));
+        });
+
+        try {
+            Map<Integer, Long> frequencyMap = frequencyMapFuture.get();
+            frequencyMap.forEach((k,v) -> System.out.println("Number : "+k+" = "+v));
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
